e4570039c135e13fadd93df4766b61407098577f
John W Higgins <wishdev@gmail.com>
2023-01-26 15:07:49 -0800

    Move the host request parsing to a separate method. (#85)
    Allows for someone to override the parsing to accommodate "alternatives".
    One could write the following to allow underscores in host names.
    
    require 'webrick/httprequest'
    
    module WEBrick
      class HTTPRequest
        private
    
        def parse_host_request_line(host, scheme)
          uri = URI.parse("#{scheme}://#{host}")
          [uri.host, uri.port]
        end
      end
    end
    
    Also adding the "o" option to the regex so the regex is only built once.

--- lib/webrick/httprequest.rb
+++ lib/webrick/httprequest.rb
@@ -491,8 +491,7 @@ module WEBrick
       if @forwarded_host
         host, port = @forwarded_host, @forwarded_port
       elsif self["host"]
-        pattern = /\A(#{URI::REGEXP::PATTERN::HOST})(?::(\d+))?\z/n
-        host, port = *self['host'].scan(pattern)[0]
+        host, port = parse_host_request_line(self["host"])
       elsif @addr.size > 0
         host, port = @addr[2], @addr[1]
       else
@@ -502,6 +501,11 @@ module WEBrick
       uri.host = host
       uri.port = port ? port.to_i : nil
       return URI::parse(uri.to_s)
+    end
+
+    def parse_host_request_line(host)
+      pattern = /\A(#{URI::REGEXP::PATTERN::HOST})(?::(\d+))?\z/no
+      host.scan(pattern)[0]
     end
 
     def read_body(socket, block)
