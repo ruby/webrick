84e865a42ea1a4a4a6a9ef5c0d136f2568333bbb
hisanori takahashi (nyan) <hisary@gmail.com>
2025-03-25 11:40:27 +0900

    Update lib/webrick/httpservlet/cgihandler.rb
    use keyword argument
    
    Co-authored-by: Jeremy Evans <code@jeremyevans.net>

--- lib/webrick/httpservlet/cgihandler.rb
+++ lib/webrick/httpservlet/cgihandler.rb
@@ -96,7 +96,7 @@ module WEBrick
           "Premature end of script headers: #{@script_filename}" if body.nil?
 
         begin
-          header = HTTPUtils::parse_header(raw_header, true)
+          header = HTTPUtils::parse_header(raw_header, cgi_mode: true)
           if /^(\d+)/ =~ header['status'][0]
             res.status = $1.to_i
             header.delete('status')
